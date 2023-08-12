af8d5116b83bb0d0e7cb03264c0a7bfe3787c50e
Yasmeen Mohammed <yasmeenshaik0786@gmail.com>
2023-08-11 22:10:26 -0400

    removed unwanted code lines

--- use_geolocation_7Aug/src/useGeolocation.js
+++ use_geolocation_7Aug/src/useGeolocation.js
@@ -1,36 +1,31 @@
 import { useState } from "react";
 
-
 export function useGeolocation() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [position, setPosition] = useState({});
-    const [error, setError] = useState(null);
-    // const { lat, lng } = position;
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
 
-    function getPosition() {
-        // setCountClicks((count) => count + 1);
+  function getPosition() {
+    // setCountClicks((count) => count + 1);
 
-        if (!navigator.geolocation)
-        return setError("Your browser does not support geolocation");
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
 
-        setIsLoading(true);
-        navigator.geolocation.getCurrentPosition(
-        (pos) => {
-            setPosition({
-            lat: pos.coords.latitude,
-            lng: pos.coords.longitude
-            });
-            setIsLoading(false);
-        },
-        (error) => {
-            setError(error.message);
-            setIsLoading(false);
-        }
-        );
-    }
-   
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
 
-    return {isLoading, position, error, getPosition}
+  return { isLoading, position, error, getPosition };
 }
-
-
